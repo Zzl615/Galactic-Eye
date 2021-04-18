f525fd455f05d9c047253b7d75059a854ab3ecbb
Noaghzil@615 <2966785870@qq.com>
2021-04-18 17:07:27 +0800

    Feat: 新增网络通知模块

---
+++ network/notify.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   notify.py
+@Time    :   2021/04/18 16:47:53
+@Author  :   Zzl
+@Contact :   noaghzil@gmail.com
+@Desc    :   订阅通知
+'''
+
+# here put the import lib
+
+
+def notify_wechat():
+    # itchat
+    pass
+
+
+def notify_alert():
+    notify_wechat()

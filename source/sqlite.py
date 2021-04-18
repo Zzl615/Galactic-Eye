a1661c756631e52f5666ae9e6012ca91d1181634
Noaghzil@615 <2966785870@qq.com>
2021-04-18 17:05:30 +0800

    Feat: 新增气象数据数据库操作

---
+++ source/sqlite.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   sqlite.py
+@Time    :   2021/04/18 16:40:13
+@Author  :   Zzl
+@Contact :   noaghzil@gmail.com
+@Desc    :   SQLite存储天气数据
+'''
+
+# here put the import lib
+
+
+class weather():
+    def get_previous_data(self):
+        pass
+
+    def save_weather_date(self):
+        pass
+
+
+def get_previous_weather_data():
+    return []
+
+
+def save_weather_date(data):
+    pass
